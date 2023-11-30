9d81e1d09297fdfb414cc5833b9241ba729f5abf
Philemon Wandera <pkwandera1@gmail.com>
2023-11-30 22:26:47 +0300

    Remove unwanted committed Remove the trial code that is no longer working on the script.

--- Rock Paper Scissors/Script.js
+++ Rock Paper Scissors/Script.js
@@ -6,32 +6,7 @@ let choice = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
     return choice[Math.floor(Math.random() * choice.length)];
 }
-// Create a fuction that shows the winer in a single round of game
-/*const playerSelection= prompt("Please enter your choice. Ether Rock, Paper or Scissors");
-const computerSelection = getComputerChoice();
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "Rock" && computerSelection === "Paper") {
-        return "You loose, Paper beats Rock";
-    } else if (playerSelection === "Rock" && computerSelection === "Rock") {
-        return "You draw";
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        return "You win, Rock beats Scissors";
-    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
-        return "You draw";
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        return "You win, Paper beats Rock";
-    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        return "You loose, Scissors beats Paper";
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        return "You win, Scissors beats Paper";
-    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        return "You loose, Rock beats Scissors";
-    } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
-        return "You draw";
-    } else return "";
-}
-console.log(playRound(playerSelection, getComputerChoice()));*/
-// create a function that calls the playRound 5 Times and declres a winner
+
     let playerScore= 0;
     let computerScore = 0;
        for (let i = 0; i < 5; i++) {
