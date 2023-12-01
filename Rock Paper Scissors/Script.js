859a7c277b1b9f859145a859044e20a698a0a7d6
Philemon Wandera <pkwandera1@gmail.com>
2023-12-01 20:07:04 +0300

    Add GUI to Rock Paper Scissors game Removed the automatic loop, replace with buttons for selecting players choice create an event fuction to generate computer choice and compare with player choice generate scores and show them on the web page.

--- Rock Paper Scissors/Script.js
+++ Rock Paper Scissors/Script.js
@@ -1,40 +1,82 @@
-// JavaScript source code
-// Create rock paper scissors game
-// create a fuction that the coputer should return rock paper scissors at random
-//
+document.addEventListener('DOMContentLoaded', function () {
 let choice = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
-    return choice[Math.floor(Math.random() * choice.length)];
+return choice[Math.floor(Math.random() * choice.length)];
+    }
+
+let playerScore= 0;
+let computerScore = 0;
+
+const rockbtn = document.querySelector('#rock');
+const paperbtn = document.querySelector('#paper');
+const scissorsbtn = document.querySelector('#scissors');
+const plscore = document.querySelector('#plscore');
+const coscore = document.querySelector('#coscore');
+
+rockbtn.addEventListener('click', () => {
+const playerSelection = "Rock"
+const computerSelection = getComputerChoice();
+
+if (computerSelection === playerSelection) {
+console.log("You draw");
+} else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+playerScore++
+    
+console.log("You win"); 
+console.log("Player " + playerScore);
+console.log("Computer " + computerScore);
+} else {
+computerScore++;
+console.log("You loose, Computer wins");
+console.log("Player " + playerScore);
+console.log("Computer " + computerScore);
+}
+    plscore.textContent = `Player Score ${playerScore}`
+    coscore.textContent = `Computer Score  ${computerScore}`          
+});
+
+    
+
+paperbtn.addEventListener('click', () => {
+const playerSelection = "Paper"
+const computerSelection = getComputerChoice();
+if (computerSelection === playerSelection) {
+console.log("You draw");
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    playerScore++
+    console.log("You win"); 
+    console.log("Player " + playerScore);
+    console.log("Computer " + computerScore);
+    } else {
+    computerScore++;
+    console.log("You loose, Computer wins");
+    console.log("Player " + playerScore);
+    console.log("Computer " + computerScore);
 }
 
-    let playerScore= 0;
-    let computerScore = 0;
-       for (let i = 0; i < 5; i++) {
-        const playerSelection= prompt("Please enter your choice. (Rock, Paper or Scissors");
-        const computerSelection = getComputerChoice();
-        console.log(`You chose ${playerSelection}`);
-        console.log(`Computer chose ${computerSelection}`);
-            if (computerSelection === playerSelection) {
-                console.log("You draw");
-            } else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
-                    (playerSelection === "Scissors" && computerSelection === "Paper") ||
-                    (playerSelection === "Paper" && computerSelection === "Rock")) {
-                        playerScore++
-                        console.log("You win"); 
-                        console.log(playerScore);
-                        console.log(computerScore);
-                    } else {
-                        computerScore++;
-                        console.log("You loose, Computer wins");
-                        console.log(playerScore);
-                        console.log(computerScore);
-                    }
-       }
-     if (computerScore < playerScore) {
-        console.log("Great play! You won");
-     } else if (computerScore > playerScore) {
-        console.log("Oh no, You lost, Computer won");
-     } else {
-        console.log("You have a Draw");
-            }
+    plscore.textContent = `Player Score ${playerScore}`
+    coscore.textContent = `Computer Score  ${computerScore}`
+}); 
+
+scissorsbtn.addEventListener('click', () => {
+const playerSelection = "Scissors"
+const computerSelection = getComputerChoice();
+if (computerSelection === playerSelection) {
+console.log("You draw");
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    playerScore++
+    console.log("You win"); 
+    console.log("Player " + playerScore);
+    console.log("Computer " + computerScore);
+    } else {
+    computerScore++;
+    console.log("You loose, Computer wins");
+    console.log("Player " + playerScore);
+    console.log("Computer " + computerScore);
+}
+
+    plscore.textContent = `Player Score ${playerScore}`
+    coscore.textContent = `Computer Score  ${computerScore}`
+});
+});
     
